62646c31bd51437b494ec4f06d73219e8fb2e3cf
Michael Lance <mlance4@uwyo.edu>
2026-06-30 11:21:37 -0500

    rebase extract code blocks to main

--- Scripts/LessonChecker.py
+++ Scripts/LessonChecker.py
@@ -9,6 +9,7 @@ from html.parser import HTMLParser
 from pathlib import Path
 import argparse
 import sys
+from typing import cast
 
 # ------------------------------------------------------------------------------------------
 # Config
@@ -21,7 +22,7 @@ opgroup.add_argument("-v", "--verify", action="store_true")
 opgroup.add_argument("-e", "--extract", action="store_true")
 opgroup.add_argument("-a", "--autofix", action="store_true")
 
-aparser.add_argument("-f", "--files", nargs="*")
+aparser.add_argument("files", nargs=argparse.REMAINDER)
 aparser.add_argument("-o", "--output")
 
 # ------------------------------------------------------------------------------------------
@@ -45,7 +46,7 @@ class SYCLAParser(HTMLParser):
         "wbr",
     }
 
-    def __init__(self, lesson_name: str, extract: bool, verify: bool):
+    def __init__(self, lesson_name: str, extract: bool, verify: bool, fix: bool):
         super().__init__(convert_charrefs=False)
 
         self.code_blocks: list[tuple[tuple[int, int], str]] = []
@@ -57,6 +58,9 @@ class SYCLAParser(HTMLParser):
 
         self.extract = extract
         self.verify = verify
+        self.fix = fix
+
+        self.msg_action = "FIXING" if self.fix else "ERROR"
 
     def handle_starttag(self, tag, attrs):
         attrd = dict(attrs)
@@ -68,20 +72,12 @@ class SYCLAParser(HTMLParser):
                 self.code_blocks.append((self.getpos(), ""))
             case "mark":
                 self.is_mark = True
-            case "span":
-                if self.is_code:
-                    self.parser_error = True
-                    if self.verify:
-                        print(
-                            f"\033[93m[WARNING] <span> used in <code> for highlight instead of <mark> in lesson {self.lesson_name} line num: {self.getpos()} \033[0m"
-                        )
             case "pre":
                 if self.is_code:
                     self.parser_error = True
-                    if self.verify:
-                        print(
-                            f"\033[93m[WARNING] misalligned <code> and <pre> tags in lesson {self.lesson_name} line num: {self.getpos()} \033[0m"
-                        )
+                    print(
+                        f"\033[93m[{self.msg_action}] misalligned <code> and <pre> tags in lesson {self.lesson_name} line num: {self.getpos()} \033[0m"
+                    )
 
                     if "code" in self.output[-1]:
 
@@ -91,17 +87,16 @@ class SYCLAParser(HTMLParser):
                 if self.is_code:
                     self.parser_error = True
                     print(
-                        f"\033[93m[WARNING] <mark> annotations are the only tags allowed in <code> blocks! Violation in lesson {self.lesson_name} line num: {self.getpos()} tag: {tag} \033[0m"
+                        f"\033[93m[{self.msg_action}] <mark> annotations are the only tags allowed in <code> blocks! Violation in lesson {self.lesson_name} line num: {self.getpos()} tag: {tag} \033[0m"
                     )
                     self.convert_charrefs = False
 
                     # Fetch the original case of the impropperly escaped code
                     raw_tag = self.get_starttag_text()
 
-                    if not raw_tag:
-                        raw_tag = tag
-
-                    self.output.append(f"&lt;{raw_tag.lstrip("<").rstrip(">")}&gt;")
+                    self.output.append(
+                        f"&lt;{cast(str, raw_tag).lstrip("<").rstrip(">")}&gt;"
+                    )
                     return
 
         tags = [tag] + [
@@ -125,17 +120,11 @@ class SYCLAParser(HTMLParser):
                 raw_entity = f"&{name};"
 
             case _:
-                raw_entity = f"&amp;{name.strip('&')}"
-
-        self.output.append(raw_entity)
-
-        if self.is_code and self.extract:
-            converted_char = html.unescape(raw_entity)
-            pos, e_data = self.code_blocks[-1]
-            self.code_blocks[-1] = (pos, e_data + converted_char)
-
-    def handle_charref(self, name):
-        raw_entity = f"&#{name};"
+                self.parser_error = True
+                print(
+                    f"\033[93m[{'FIXING' if self.fix else 'ERROR'}] Impropperly escaped entity ref! Violation in lesson {self.lesson_name} line num: {self.getpos()} ref: {name} \033[0m"
+                )
+                raw_entity = f"&amp;{name}"
 
         self.output.append(raw_entity)
 
@@ -155,6 +144,10 @@ class SYCLAParser(HTMLParser):
 
     def handle_endtag(self, tag):
         if tag.lower() in self.VOID_ELEMENTS:
+            self.parser_error = True
+            print(
+                f"\033[93m[{'FIXING' if self.fix else 'ERROR'}] Self closing tag does not require an endtag Violation in lesson {self.lesson_name} line num: {self.getpos()} tag: {tag} \033[0m"
+            )
             return
 
         if tag.lower() == "code":
@@ -169,11 +162,26 @@ class SYCLAParser(HTMLParser):
     def handle_decl(self, decl):
         self.output.append(f"<!{decl}>")
 
+    def get_output(self):
+        return "".join(self.output)
+
+    def get_codeblocks(self):
+        return self.code_blocks
 
 # ------------------------------------------------------------------------------------------
 
 
-if __name__ == "__main__":
+def verify_html(lesson, file_str, args) -> tuple[str, list, bool]:
+    parser = SYCLAParser(lesson, args.extract, args.verify, args.autofix)
+    parser.feed(file_str)
+
+    return parser.get_output(), parser.get_codeblocks(), parser.parser_error
+
+
+# ------------------------------------------------------------------------------------------
+
+
+if __name__ == "__main__":  # pragma: no cover
     args = aparser.parse_args()
 
     if not any([args.extract, args.autofix, args.verify]):
@@ -188,15 +196,14 @@ if __name__ == "__main__":
     for lesson in args.files:
         print(f"processing lesson: {lesson}...")
         l_path = Path(lesson)
+        print(f"l_path: {l_path.parent.name}")
 
-        in_path = l_path / "index.html"
-
-        if not in_path.exists():
+        if not l_path.exists():
             msg = f'Lesson "{lesson}" does not exist!'
             raise Exception(msg)
 
-        with open(in_path, "r", encoding="utf-8") as file:
-            parser = SYCLAParser(lesson, args.extract, args.verify)
+        with open(l_path, "r", encoding="utf-8") as file:
+            parser = SYCLAParser(lesson, args.extract, args.verify, args.autofix)
             parser.feed(file.read())
 
             if not parser.parser_error:
@@ -206,14 +213,14 @@ if __name__ == "__main__":
                 out_base.mkdir(parents=True, exist_ok=True)
 
                 for pos, code in parser.code_blocks:
-                    name = f"{out_base}/{l_path.name}-{pos[0]}-{pos[1]}.cpp"
+                    name = f"{out_base}/{l_path.parent.name}-{pos[0]}-{pos[1]}.cpp"
 
                     print(f"Exporting script: {name}")
                     with open(name, "w") as out:
                         out.write(code)
 
             if args.autofix:
-                write_back_path = in_path
+                write_back_path = l_path
 
                 with open(write_back_path, "w") as out:
-                    out.write("".join(parser.output))
+                    out.write(parser.get_output())

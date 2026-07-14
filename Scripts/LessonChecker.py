a32d3fc894f1be7641c8029c114f93957adbf34e
Michael Lance <83855101+MikeL270@users.noreply.github.com>
2026-07-14 12:01:38 -0500

    Extract, compile, and run examples from slides in CI (#428)
    * add extractor script
    
    * add file name and trace to warning
    
    * small fixes
    
    * merge extractor and parsers
    
    * save fixes to disk
    
    * remove parser.py
    
    * add test coverage for parser
    
    * fix comments and declarations
    
    * handle declarations, fix charrefs, and tests
    
    * add extractor script
    
    * add file name and trace to warning
    
    * small fixes
    
    * rebase extract code blocks to main
    
    * use full file path
    
    * full test coverage for parser
    
    * build code examples with acpp
    
    * test coverage for script extractor and coherent extracted code block names
    
    * fix apt cache and 100% gate
    
    * appropriate messages for fixing / verifying
    
    * attempted fix for cache permission denied
    
    * run code block snippets in ci
    
    * generate testfile to convince ctest to actually do something
    
    * remove gpu selector so that code examples can run in CI
    
    * remove parsers.py, again.
    
    * add verify gate for example lesson
    
    * fold lesson code into existing cmake based workflow
    
    * refresh apt packages in cache
    
    * move apt update outside of its own step
    
    * remove lesson_snippets to verify failure source
    
    * add missing lld dependency
    
    * remove unessacary selectors
    
    * fix syntax error
    
    * remove nonsense comment left by claude
    
    * set up python in more reasonable spot in workflow
    
    * keep cpu selector
    
    * squash runners and lessons in openmp too
    
    * small fixes to extractor
    
    * create cmakelists in extractor script
    
    * fix extension in cmakelists
    
    * rename steps to be more clear
    
    * silent mode, enum state, some code flattening
    
    * adhere to HTMLParser with output
    
    * restore 100% test cov
    
    * fix ampersand escape issue, update tests
    
    * comments, better mode unloading
    
    * remove debug statement opps

---
+++ Scripts/LessonChecker.py
@@ -0,0 +1,285 @@
+# SYCL academy lesson checker and code extractor
+# Michael Lance
+# July 2026
+
+# ------------------------------------------------------------------------------------------
+
+import html
+from html.parser import HTMLParser
+from dataclasses import dataclass, field
+from enum import IntFlag, auto
+from pathlib import Path
+import argparse
+import sys
+
+# ------------------------------------------------------------------------------------------
+# Config
+
+aparser = argparse.ArgumentParser()
+
+opgroup = aparser.add_mutually_exclusive_group()
+
+# Autofix and extract operations are always performed, these flags control whether its written to disk
+opgroup.add_argument("-v", "--verify", action="store_true")
+opgroup.add_argument("-e", "--extract", action="store_true")
+opgroup.add_argument("-a", "--autofix", action="store_true")
+
+aparser.add_argument("files", nargs=argparse.REMAINDER)
+aparser.add_argument("-o", "--output")
+aparser.add_argument("-s", "--silent", action="store_true")
+
+# ANSI color codes for output
+RED = "\033[31m"
+GREEN = "\033[32m"
+YELLOW = "\033[33m"
+RESET = "\033[0m"
+
+# ------------------------------------------------------------------------------------------
+
+
+class Mode(IntFlag):
+    NONE = 0
+    EXTRACT = auto()
+    VERIFY = auto()
+    AUTOFIX = auto()
+    SILENT = auto()
+
+
+@dataclass
+class SYCLAParser(HTMLParser):
+    lesson_name: str
+    mode: Mode
+    code_blocks: list[tuple[int, str]] = field(default_factory=list)
+    output: list[str] = field(default_factory=list)
+    is_code: bool = False
+    is_mark: bool = False
+    parser_error: bool = False
+
+    SELF_CLOSING_TAGS = {
+        "area",
+        "base",
+        "br",
+        "col",
+        "embed",
+        "hr",
+        "img",
+        "input",
+        "link",
+        "meta",
+        "param",
+        "source",
+        "track",
+        "wbr",
+    }
+
+    COLORS = {Mode.VERIFY: YELLOW, Mode.AUTOFIX: GREEN}
+
+    def __post_init__(self):
+        # call the constructor for the parent class
+        # Tell the HTMLParser base class to not convert character references
+        super().__init__(convert_charrefs=False)
+
+    @property
+    def msg_action(self):
+        return "FIXING" if self.mode & Mode.AUTOFIX else "ERROR"
+
+    @property
+    def output_joined(self):
+        return "".join(self.output)
+
+    def _encode_attrs(self, tag, attrs):
+        tag_attrs = [tag] + [
+            f"{k}" if v in [None, True] else f'{k}="{v.strip()}"'  # pyright: ignore
+            for k, v in attrs
+        ]
+
+        if tag in self.SELF_CLOSING_TAGS:
+            return f"<{' '.join(tag_attrs)}/>"
+        else:
+            return f"<{' '.join(tag_attrs)}>"
+
+    def _warn(self, message):
+        if self.mode & Mode.SILENT:
+            return
+
+        if bool(self.mode & (Mode.AUTOFIX | Mode.VERIFY)):
+            print(f"{self.COLORS[self.mode]}[{self.msg_action}] {message} {RESET}")
+
+    def handle_starttag(self, tag, attrs):
+        match tag:
+            case "code":
+                self.is_code = True
+                if ("class", "language-cpp") not in attrs:
+                    attrs.append(("class", "language-cpp"))
+
+                self.code_blocks.append((self.getpos()[0], ""))
+
+            case "mark":
+                self.is_mark = True
+
+            case "pre" if self.is_code:
+
+                self.parser_error = True
+                self._warn(
+                    f"misalligned <code> and <pre> tags in lesson {self.lesson_name} line num: {self.getpos()[0]}"
+                )
+
+                if "code" in self.output[-1]:
+                    self.output.insert(-1, "<pre>")
+                    return
+
+            case _:
+                # Any other unescaped
+                if self.is_code:
+                    self.parser_error = True
+                    self._warn(
+                        f"<mark> annotations are the only tags allowed in <code> blocks! Violation in lesson {self.lesson_name} line num: {self.getpos()} tag: {tag}"
+                    )
+
+                    # Fetch the original case of the impropperly escaped code
+                    raw_tag = self.get_starttag_text()
+
+                    self.output.append(
+                        f"&lt;{raw_tag.lstrip("<").rstrip(">")}&gt;"  # pyright: ignore
+                    )
+                    return
+
+        self.output.append(self._encode_attrs(tag, attrs))
+
+    def handle_data(self, data):
+        if self.is_code and bool(self.mode & Mode.EXTRACT):
+            pos, e_data = self.code_blocks[-1]
+            self.code_blocks[-1] = (pos, e_data + data)
+
+        if data == "&":
+            self.parser_error = True
+            self._warn(
+                f"Unescaped &! Violation in lesson {self.lesson_name} line num: {self.getpos()}"
+            )
+            data = "&amp;"
+
+        self.output.append(data)
+
+    def handle_entityref(self, name):
+
+        match name:
+            case "lt" | "gt" | "amp":
+                raw_entity = f"&{name};"
+            case _:
+                self.parser_error = True
+                self._warn(
+                    f"Impropperly escaped entity ref! Violation in lesson {self.lesson_name} line num: {self.getpos()} ref: {name}"
+                )
+
+                raw_entity = f"&amp;{name}"
+
+        self.output.append(raw_entity)
+
+        if self.is_code and bool(self.mode & Mode.EXTRACT):
+            converted_char = html.unescape(raw_entity)
+            pos, e_data = self.code_blocks[-1]
+            self.code_blocks[-1] = (pos, e_data + converted_char)
+
+    def handle_startendtag(self, tag, attrs):
+        self.output.append(self._encode_attrs(tag, attrs))
+
+    def handle_endtag(self, tag):
+        if tag in self.SELF_CLOSING_TAGS:
+            self.parser_error = True
+            self._warn(
+                f"Self closing tag does not require an endtag Violation in lesson {self.lesson_name} line num: {self.getpos()} tag: {tag}"
+            )
+            return
+
+        if tag == "code":
+            self.is_code = False
+            self.is_mark = False
+
+        self.output.append(f"</{tag}>")
+
+    def handle_comment(self, data):
+        self.output.append(f"<!--{data}-->")
+
+    def handle_decl(self, decl):
+        self.output.append(f"<!{decl}>")
+
+
+# ------------------------------------------------------------------------------------------
+
+
+def verify_html(lesson, file_str, args) -> tuple[str, list, bool]:
+    mode = Mode.NONE
+
+    for key, value in vars(args).items():
+        if value and key not in {"files", "output"}:
+            mode |= getattr(Mode, key.upper())
+
+    parser = SYCLAParser(lesson, mode)
+    parser.feed(file_str)
+
+    return parser.output_joined, parser.code_blocks, parser.parser_error
+
+
+# ------------------------------------------------------------------------------------------
+
+
+if __name__ == "__main__":  # pragma: no cover
+    args = aparser.parse_args()
+
+    if not any([args.extract, args.autofix, args.verify]):
+        print("No arguments provided. There is nothing to do.")
+        sys.exit(1)
+
+    if args.extract and not args.output:
+        raise Exception("Error: An output must be specified with -o or --output")
+
+    out_base = Path(args.output or "")
+    cmake_executables = ""
+
+    verify_failed = False
+
+    for lesson in args.files:
+        print(f"processing lesson: {lesson}...")
+        l_path = Path(lesson)
+
+        if not l_path.exists():
+            msg = f'Lesson "{lesson}" does not exist!'
+            raise Exception(msg)
+
+        with open(l_path, "r", encoding="utf-8") as file:
+
+            output, code_blocks, error = verify_html(lesson, file.read(), args)
+
+            if args.verify:
+                if error:
+                    verify_failed = True
+                else:
+                    print("No issues found.")
+
+            if args.extract:
+                out_base.mkdir(parents=True, exist_ok=True)
+
+                for pos, code in code_blocks:
+                    name = f"{l_path.parent.name}-l{pos}"
+
+                    cmake_executables += (
+                        f"add_sycl_executable(Lesson_Snippets {name})\n"
+                    )
+
+                    wpath = f"{out_base}/{name}.cpp"
+
+                    print(f"Exporting script: {wpath}")
+                    with open(wpath, "w") as out:
+                        out.write(code)
+
+            with open(f"{out_base}/CMakeLists.txt", "w") as cmake:
+                cmake.write(cmake_executables)
+
+            if args.autofix:
+                write_back_path = l_path
+
+                with open(write_back_path, "w") as out:
+                    out.write(output)
+
+    if verify_failed:
+        sys.exit(1)
---
+++ Scripts/test_LessonChecker.py
@@ -0,0 +1,185 @@
+import argparse
+import unittest
+import LessonChecker
+
+test_cases = [
+    {
+        "name": "detect_swapped_code_pre",
+        "body": "<code><pre></code></pre>",
+        "extract": False,
+        "verify": True,
+        "fix": False,
+        "silent": True,
+        "expected_return": True,
+    },
+    {
+        "name": "detect_illegal_tag_in_code",
+        "body": "<pre><code><span></code></pre>",
+        "extract": False,
+        "verify": True,
+        "fix": False,
+        "silent": True,
+        "expected_return": True,
+    },
+    {
+        "name": "detect_mark_in_code",
+        "body": "<pre><code><mark>message</mark></code><pre>",
+        "extract": False,
+        "verify": True,
+        "fix": False,
+        "silent": True,
+        "expected_return": False,
+    },
+    {
+        "name": "parse_start_end_tag",
+        "body": "<div><img /></div>",
+        "extract": False,
+        "verify": True,
+        "fix": False,
+        "silent": True,
+        "expected_return": False,
+    },
+    {
+        "name": "parse_start_end_tag_silent",
+        "body": "<div><img /></div>",
+        "extract": False,
+        "verify": True,
+        "fix": False,
+        "silent": True,
+        "expected_return": False,
+    },
+    {
+        "name": "parse_self_closing_tag",
+        "body": "<html><head><link></link></head><html>",
+        "extract": False,
+        "verify": True,
+        "fix": False,
+        "silent": True,
+        "expected_return": True,
+    },
+    {
+        "name": "validate_comment",
+        "body": "<!--comment--><div><p>content</p></div>",
+        "extract": False,
+        "verify": True,
+        "fix": False,
+        "silent": True,
+        "expected_return": False,
+    },
+    {
+        "name": "validate_decl",
+        "body": "<!DOCTYPE html>",
+        "extract": False,
+        "verify": True,
+        "fix": False,
+        "silent": True,
+        "expected_return": False,
+    },
+    {
+        "name": "extract_code_block",
+        "body": "<pre><code>int main() { return 0; }</code></pre>",
+        "extract": True,
+        "verify": False,
+        "fix": False,
+        "silent": True,
+        "expected_return": False,
+        "expected_code": "int main() { return 0; }",
+    },
+    {
+        "name": "extract_entity_ref_in_code_block",
+        "body": "<pre><code>&evt</code></pre>",
+        "extract": True,
+        "verify": False,
+        "fix": False,
+        "silent": True,
+        "expected_return": True,
+        "expected_code": "&evt",
+    },
+    {
+        "name": "extract_proper_lt_and_gt",
+        "body": "<pre><code>#include&lt;sycl/sycl.hpp&gt;</code></pre>",
+        "extract": True,
+        "verify": False,
+        "fix": False,
+        "silent": True,
+        "expected_return": False,
+        "expected_code": "#include<sycl/sycl.hpp>",
+    },
+    {
+        "name": "fix_mismatched_pre_code",
+        "body": "<code><pre></code></pre>",
+        "extract": False,
+        "verify": False,
+        "fix": True,
+        "silent": True,
+        "expected_return": True,
+        "expected_output": '<pre><code class="language-cpp"></code></pre>',
+    },
+    {
+        "name": "fix_improper_escaped_ref",
+        "body": '<pre><code class="language-cpp">&evt</code></pre>',
+        "extract": False,
+        "verify": False,
+        "fix": True,
+        "silent": True,
+        "expected_return": True,
+        "expected_output": '<pre><code class="language-cpp">&amp;evt</code></pre>',
+    },
+    {
+        "name": "fix_improper_escaped_gators",
+        "body": '<pre><code class="language-cpp">#include<sycl/sycl.hpp></code></pre>',
+        "extract": False,
+        "verify": False,
+        "fix": True,
+        "silent": False,
+        "expected_return": True,
+        "expected_output": '<pre><code class="language-cpp">#include&lt;sycl/sycl.hpp&gt;</code></pre>',
+    },
+    {
+        "name": "fix_improper_escaped_ampersand",
+        "body": '<pre><code class="language-cpp">&</code></pre>',
+        "extract": False,
+        "verify": False,
+        "fix": False,
+        "silent": False,
+        "expected_return": True,
+        "expected_output": '<pre><code class="language-cpp">&amp;</code></pre>',
+    },
+]
+
+
+def make_test_method(case_data):
+    def test_method(self):
+        args = argparse.Namespace(
+            extract=case_data["extract"],
+            verify=case_data["verify"],
+            autofix=case_data["fix"],
+            silent=case_data["silent"],
+        )
+
+        output, codes, error = LessonChecker.verify_html(
+            "test_lesson", case_data["body"], args
+        )
+
+        self.assertEqual(error, case_data["expected_return"])
+
+        if case_data["extract"] and "expected_code" in case_data:
+            # Assume only one code block per test
+            self.assertEqual(len(codes), 1)
+
+            code = codes.pop()
+            self.assertEqual(code[1], case_data["expected_code"])
+
+        if case_data["fix"] and "expected_output" in case_data:
+            self.assertEqual(output, case_data["expected_output"])
+
+    return test_method
+
+
+class TestSYCLAParser(unittest.TestCase):
+    pass
+
+
+for case in test_cases:
+    test_method_name = f"test_{case['name']}"
+    setattr(TestSYCLAParser, test_method_name, make_test_method(case))

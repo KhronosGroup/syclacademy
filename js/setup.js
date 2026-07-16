8c8965fb278558ea500f868f973f4e3cda501557
Michael Lance <mlance4@uwyo.edu>
2026-07-16 17:59:08 -0500

    remove npm dependence and revert to branch based pages

--- js/setup.js
+++ js/setup.js
@@ -4,10 +4,10 @@
 // slides also work when opened directly from the file system), then includes
 // this script last:
 //
-//   <script src="../../node_modules/reveal.js/dist/reveal.js"></script>
-//   <script src="../../node_modules/reveal.js/dist/plugin/markdown.js"></script>
-//   <script src="../../node_modules/reveal.js/dist/plugin/highlight.js"></script>
-//   <script src="../../node_modules/reveal.js/dist/plugin/notes.js"></script>
+//   <script src="../../js/revealjs/reveal.js"></script>
+//   <script src="../../js/revealjs/plugin/markdown.js"></script>
+//   <script src="../../js/revealjs/plugin/highlight.js"></script>
+//   <script src="../../js/revealjs/plugin/notes.js"></script>
 //   <script src="../../js/setup.js"></script>
 //
 // This file removes the per-lesson duplication of the shared slide styling,
